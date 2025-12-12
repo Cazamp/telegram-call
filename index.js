45e8dd6baa70ee469569126a909b02b839bd9e49
Cazamp <juancamilozp0@gmail.com>
2025-12-12 14:23:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 require("dotenv").config();
+console.log("TWILIO_ACCOUNT_SID:", process.env.TWILIO_ACCOUNT_SID ? "OK" : "MISSING");
+console.log("TWILIO_AUTH_TOKEN:", process.env.TWILIO_AUTH_TOKEN ? "OK" : "MISSING");
+console.log("TWILIO_NUMBER:", process.env.TWILIO_NUMBER || "MISSING");
+console.log("TO_NUMBER:", process.env.TO_NUMBER || "MISSING");
+
 console.log(
   "TOKEN:",
   process.env.TELEGRAM_BOT_TOKEN ? "OK" : "MISSING"
